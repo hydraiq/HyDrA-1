1e94d77fb9680ff709e46a23938e0fa917505c6b
HyDrASM <84361933+HyDrASM@users.noreply.github.com>
2021-05-20 02:40:58 +0300

    Update Fastinstall.sh

--- Fastinstall.sh
+++ Fastinstall.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-cd $HOME/TshAkE
+cd $HOME/HyDrA
 install() {
 rm -rf $HOME/.telegram-cli
 sudo chmod +x tg
-chmod +x Tshake
+chmod +x HyDrA
 chmod +x install.sh
 chmod +x ts
 ./ts
 }
 get() {
-rm -fr Tshake.lua
+rm -fr HyDrA.lua
 rm -fr sudo.lua
-wget "https://raw.githubusercontent.com/TshAkEAb/TshAkE/master/Tshake.lua"
+wget "https://raw.githubusercontent.com/HyDrASM/HyDrA/master/HyDrA.lua"
 lua start.lua
 }
 installall(){
@@ -48,7 +48,7 @@ fi
 installall
 cd ..
 rm -rf luarocks*
-cd TshAkE
+cd HyDrA
 rm -rf luarocks*
 lua start.lua
 
